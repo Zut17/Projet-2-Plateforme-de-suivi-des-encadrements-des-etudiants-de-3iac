15aa9db9325d19d9e06a83f0a8d551dddd859793
@MasterZ17 <zuteuhenrywilliam@gmail.com>
2026-01-03 17:17:12 +0100

    modifstruct

--- plateformsuivie/src/App.jsx
+++ plateformsuivie/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Login from './pages/Login'
+import Login from './pagesLogin/Login'
 
 function App() {
  
