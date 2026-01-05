27b6d0c8a0103b017dc4190013261c07ae00ad5b
@MasterZ17 <zuteuhenrywilliam@gmail.com>
2026-01-05 10:38:48 +0100

    modification login

--- plateformsuivie/src/pagesLogin/Login.jsx
+++ plateformsuivie/src/pagesLogin/Login.jsx
@@ -2,15 +2,17 @@ import "../styles/login.css";
 
 function Login() {
   return (
+    <>
     <div className="login-container">
       <h2>Connexion</h2>
-
       <form className="login-form">
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Mot de passe" />
         <button type="submit">Se connecter</button>
+        <a href="#">Mot de passe oublié ?</a>
       </form>
     </div>
+    </>
   );
 }
 
